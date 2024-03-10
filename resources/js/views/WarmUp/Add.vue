751b0ce297758e0410580e7f770adc11dbf22759
cristhian-ic <126696151+cristhian-ic@users.noreply.github.com>
2024-03-10 17:38:30 -0500

    fix: front

--- resources/js/views/WarmUp/Add.vue
+++ resources/js/views/WarmUp/Add.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>Creating New WarmUp</div>
+    <div>Creating New</div>
     <div class="form-group">
         <v-form ref="form" fast-fail @submit.prevent="createNewItem()">
             <label for="quantity">Name:</label>
